46b9fdec3925ddf09475b61fd4f3cc4a2a88a6d9
shreyaskar123 <47864384+shreyaskar123@users.noreply.github.com>
2023-01-16 17:56:58 -0500

    Update model.py

--- ai/model.py
+++ ai/model.py
@@ -1,16 +1,31 @@
 import os
 import openai
 
-openai.api_key = "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"
+openai.api_key = "API_KEY"
 
 
-p = "Write Java code to "
-file = open('story.txt', 'r')
+p = "Provide a summary of what is expected to be completed:"
+file = open('title.txt', 'r')
 for line in file:
-    p += line
+    p+= line
 response = openai.Completion.create(
     model="text-davinci-003",
     prompt=p,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+)
+print(response.choices[-1].text)
+k = f"Context:{p} \n Now please complete the following code in java: \n"
+
+file = open('story.txt', 'r')
+for line in file:
+    k+= line
+response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=k,
     temperature=0.7,
     max_tokens=256,
     top_p=1,
